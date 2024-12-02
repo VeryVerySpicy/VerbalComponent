28ed9a1b5461f14b78d876b8b37576adcdf26111
Ink-WeiZhang <ink.wj.zhang@gmail.com>
2024-12-01 20:49:32 -0800

    Final features

--- app/src/main/java/com/example/verbalcomponentjava/MainActivity.java
+++ app/src/main/java/com/example/verbalcomponentjava/MainActivity.java
@@ -11,7 +11,6 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Handler;
 import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
@@ -23,6 +22,7 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Locale;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,15 +33,20 @@ public class MainActivity extends AppCompatActivity {
     private TextView textView;
     private Handler handler;
 
-    private String GAME_STATE_SAFE_0 = "The Way is Clear";
-    private String GAME_STATE_SAFE_1 = "The slime was burnt to a crisp!";
-    private String GAME_STATE_DANGER_0 = "A slime is approaching!";
-    private String GAME_STATE_DANGER_1 = "You were eaten by a slime!";
+    private int SLIMES_KILLED = 0;
+    private int DELAY_TIME = 3000;
 
-    private String GAME_STATE_TUTORIAL_0 = "Oh no! There are slimes on the road back to the wizard's tower.";
-    private String GAME_STATE_TUTORIAL_1 = "Hold down the wand icon to start chanting, release when you're done.";
-    private String GAME_STATE_TUTORIAL_2 = "'Fireball' kills blue slimes, 'Icebolt' kills red slimes'";
+    private final String GAME_STATE_SAFE_0 = "The Way is Clear";
+    private final String GAME_STATE_SAFE_1 = "The slime was slain!";
+    private final String GAME_STATE_DANGER_0_0 = "A red slime is approaching!";
+    private final String GAME_STATE_DANGER_0_1 = "A blue slime is approaching!";
+    private final String GAME_STATE_DANGER_1 = "You were eaten by a slime!";
 
+    private final String GAME_STATE_TUTORIAL_0 = "Oh no! There are slimes on the road back to the wizard's tower.";
+    private final String GAME_STATE_TUTORIAL_1 = "Hold down the wand icon to start chanting, release when you're done.";
+    private final String GAME_STATE_TUTORIAL_2 = "'Fire' kills blue slimes, 'Ice' kills red slimes'";
+    private final String GAME_STATE_VICTORY = "You've made it back to your tower! Congradulations!";
+    Random rand;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,12 +56,14 @@ public class MainActivity extends AppCompatActivity {
             requestPermission();
         }
 
+        rand = new Random();
+
         editText = findViewById(R.id.edtSpeechText);
         wandBtn = findViewById(R.id.wandBtn);
         textView = findViewById(R.id.the_path);
 
         handler = new Handler();
-        handler.postDelayed(runnable, 5000);
+        handler.postDelayed(runnable, DELAY_TIME);
 
         speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
 
@@ -94,7 +101,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResults(Bundle bundle) {
                 ArrayList<String> data = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
-                textView.setText(GAME_STATE_SAFE_1);
+                assert data != null;
+                if ((textView.getText().equals(GAME_STATE_DANGER_0_0) && data.get(0).equalsIgnoreCase("Ice"))
+                        || (textView.getText().equals(GAME_STATE_DANGER_0_1) && data.get(0).equalsIgnoreCase("Fire"))) {
+                    SLIMES_KILLED++;
+                    textView.setText(GAME_STATE_SAFE_1);
+
+                }
                 editText.setText(data.get(0));
             }
 
@@ -106,7 +119,7 @@ public class MainActivity extends AppCompatActivity {
             public void onEvent(int i, Bundle bundle) {
             }
         });
-
+        wandBtn.setImageResource(R.drawable.wand);
         wandBtn.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
@@ -137,9 +150,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void requestPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, AUDIO_PERMISSION);
-        }
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, AUDIO_PERMISSION);
     }
 
     private boolean isPermissionGranted() {
@@ -160,23 +171,45 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void run() {
             String currentString = textView.getText().toString();
-            if (currentString.equals(GAME_STATE_SAFE_0)) {
-                currentString = GAME_STATE_DANGER_0;
-            } else if (currentString.equals(GAME_STATE_SAFE_1)) {
-                currentString = GAME_STATE_SAFE_0;
-            } else if (currentString.equals(GAME_STATE_DANGER_0)) {
-                currentString = GAME_STATE_DANGER_1;
-            }
-            final String tempString = currentString;
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    textView.setText(tempString);
+            if (SLIMES_KILLED >= 3) {
+                textView.setText(GAME_STATE_VICTORY);
+            } else {
+                switch (currentString) {
+                    case GAME_STATE_TUTORIAL_0:
+                        currentString = GAME_STATE_TUTORIAL_1;
+                        break;
+                    case GAME_STATE_TUTORIAL_1:
+                        currentString = GAME_STATE_TUTORIAL_2;
+                        break;
+                    case GAME_STATE_TUTORIAL_2:
+                        currentString = GAME_STATE_SAFE_0;
+                        DELAY_TIME = 5000;
+                        break;
+                    case GAME_STATE_SAFE_0:
+                        if (rand.nextBoolean()) {
+                            currentString = GAME_STATE_DANGER_0_0;
+                        } else {
+                            currentString = GAME_STATE_DANGER_0_1;
+                        }
+                        break;
+                    case GAME_STATE_SAFE_1:
+                        currentString = GAME_STATE_SAFE_0;
+                        break;
+                    case GAME_STATE_DANGER_0_0:
+                    case GAME_STATE_DANGER_0_1:
+                        currentString = GAME_STATE_DANGER_1;
+                        break;
                 }
-            });
-            //Change the textview again after 5 seconds
-            handler.postDelayed(runnable, 5000);
+                final String tempString = currentString;
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        textView.setText(tempString);
+                    }
+                });
+                //Change the textview again after 5 seconds
+                handler.postDelayed(runnable, DELAY_TIME);
+            }
         }
     };
-
 }
